1672ce026e2ab2a5198dc9819b09cdd8103e914c
Adam Bławat <blawat.adam@gmail.com>
2022-08-30 17:36:47 +0200

    Add RTC Function with Callback
    Created RTC callback function.
    Added multiple alarms setting.

--- router.c
+++ router.c
@@ -8,18 +8,52 @@
 #include "pico/stdlib.h"
 #include "pico/util/datetime.h"
 
-#define PICO_LED_PIN 2
+#define PICO_DEBUG_PIN 2
+
+static volatile bool fired = false;
+
+/* Set alarm */
+static datetime_t alarm =
+{
+    .year  = -1,
+    .month = -1,
+    .day   = -1,
+    .dotw  = -1,
+    .hour  = 0,
+    .min   = 2,
+    .sec   = 0
+};
+
+
+static void alarm_callback(void)
+{
+    alarm.min += 2;
+
+    rtc_set_alarm(&alarm, &alarm_callback);
+
+    if (fired == true)
+    {
+        gpio_put(PICO_DEBUG_PIN, 0);
+        fired = false;
+    }
+    else
+    {
+        gpio_put(PICO_DEBUG_PIN, 1);
+        fired = true;
+    }
+}
 
 int main()
 {
-    /* Start on Friday 24th of August 2022 11:00:00 */
-    datetime_t time = {
+    /* Start on Wednesday 24th of August 2022 15:30:00 */
+    datetime_t time =
+    {
             .year  = 2022,
             .month = 8,
             .day   = 24,
-            .dotw  = 3,       /* 0 is Sunday */
-            .hour  = 15,
-            .min   = 30,
+            .dotw  = 3,
+            .hour  = 23,
+            .min   = 58,
             .sec   = 0
     };
 
@@ -30,25 +64,14 @@ int main()
     /* 3 RTC clock cycles delay (which is 64us with the default clock settings) */
     sleep_us(64);
 
-    const uint LED_PIN = PICO_LED_PIN;
+    rtc_set_alarm(&alarm, &alarm_callback);
 
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_init(PICO_DEBUG_PIN);
+    gpio_set_dir(PICO_DEBUG_PIN, GPIO_OUT);
 
-    while (true)
-    {
-        rtc_get_datetime(&time);
+    gpio_put(PICO_DEBUG_PIN, 0);
 
-	if (((time.hour >= 15) && (time.hour < 16)) ||
-            ((time.hour >= 17) && (time.hour < 18)))
-	{
-            gpio_put(LED_PIN, 1);
-	}
-	else
-	{
-            gpio_put(LED_PIN, 0);
-	}
+    while (true);
 
-        sleep_ms(1000);
-    }
+    return 0;
 }
